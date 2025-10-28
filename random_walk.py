30e222fff83213b98ba3b1afafc6efbc46764ba2
Annik140 <Annik140>
2025-10-28 15:58:05 +0100

    pylint fix random_Walk

--- random_walk.py
+++ random_walk.py
@@ -1,71 +1,100 @@
-# This is our implementation of the walkers without using numpy
+"""random walk, task 1 in topic 2, project 3 MOD300"""
 import random
 from matplotlib import pyplot as plt
 
-class point:
+# This is our implementation of the walkers without using numpy
+
+class Point:
+    """
+    Point in 3D space
+    """
     def __init__(self, x=0.0, y=0.0, z=0.0):
         self.x = x
         self.y = y
         self.z = z
 
     def set_point(self, x, y, z=0.0):
+        """
+        Set the point coordinates
+        """
         self.x = x
         self.y = y
         self.z = z
 
     def get_point(self):
+        """
+        Get the point coordinates
+        """
         return (self.x, self.y, self.z)
 
     def add_point(self, other_point):
+        """
+        Add another point to this point
+        """
         self.x += other_point.x
         self.y += other_point.y
         self.z += other_point.z
 
 
-class walker:
+class Walker:
+    """
+    A walker in 3D space
+    """
     def __init__(self, x=0, y=0, z=0):
-        self.position = point(x, y, z)
-        self.velocity = point()
+        self.position = Point(x, y, z)
+        self.velocity = Point()
         self.past_positions = []
 
-    def move(self, point_change: point):
+    def move(self, point_change: Point):
+        """
+        Move the walker by a point
+        """
         self.past_positions.append(self.position.get_point())
         self.position.add_point(point_change)
 
     def get_position(self):
+        """
+        Get the position of the walker
+        """
         return self.position.get_point()
-    
+
 
 def random_point(min_val=1, max_val=1):
+    """
+    Generate a random point in 3D space within given range
+    """
     x = random.uniform(min_val, max_val)
     y = random.uniform(min_val, max_val)
     z = random.uniform(min_val, max_val)
-    return point(x, y, z)
+    return Point(x, y, z)
 
 
 def gen_walkers(iterations, walker_count, start_min=-100, start_max=100):
-    walkers = []
-    for i in range(walker_count):
+    """
+    Generate a list of walkers and move them randomly
+    """
+    walkers_list = []
+    for indx in range(walker_count):
         start = random_point(start_min, start_max)
-        w = walker(start.x, start.y, start.z)
+        walker = Walker(start.x, start.y, start.z)
         # testing random starting points
-        print(f"Walker {i+1} start: {start.x}, {start.y}, {start.z}")
-        for j in range(iterations):
+        print(f"Walker {indx+1} start: {start.x}, {start.y}, {start.z}")
+        for _ in range(iterations):
             step = random_point(-1, 1)
-            w.move(step)
-        walkers.append(w)
-
-    return walkers
+            walker.move(step)
+        walkers_list.append(walker)
+    return walkers_list
 
 
 if __name__ == "__main__":
-    iterations = 10000
-    walker_count = 5
+    ITERATIONS = 10000
+    WALKER_COUNT = 5
 
-    walkers = gen_walkers(iterations, walker_count)
+    walkers = gen_walkers(ITERATIONS, WALKER_COUNT)
 
     # Plotting the random walk
-    # Source for plot code: https://stackoverflow.com/questions/11541123/how-can-i-make-a-3d-line-plot
+    # Source for plot code:
+    # https://stackoverflow.com/questions/11541123/how-can-i-make-a-3d-line-plot
     fig = plt.figure()
     ax = fig.add_subplot(111, projection='3d')
 
